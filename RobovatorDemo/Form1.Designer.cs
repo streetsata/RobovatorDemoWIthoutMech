dae51ddcf816ad0ff337c08589b8471aff43ed77
Serhiy Artemenko <serhiy_artemenko@outllok.com>
2015-05-06 01:17:34 +0300

    Add installer, filters, etc

--- RobovatorDemo/Form1.Designer.cs
+++ RobovatorDemo/Form1.Designer.cs
@@ -31,7 +31,13 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
-            this.videoSourcePlayer2 = new AForge.Controls.VideoSourcePlayer();
+            this.pictureBox1 = new AForge.Controls.PictureBox();
+            this.sliderControl1 = new AForge.Controls.SliderControl();
+            this.colorSlider1 = new AForge.Controls.ColorSlider();
+            this.sliderControl2 = new AForge.Controls.SliderControl();
+            this.sliderControl3 = new AForge.Controls.SliderControl();
+            this.sliderControl4 = new AForge.Controls.SliderControl();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -50,32 +56,94 @@
             // 
             // videoSourcePlayer1
             // 
-            this.videoSourcePlayer1.Location = new System.Drawing.Point(0, 43);
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(12, 42);
             this.videoSourcePlayer1.Name = "videoSourcePlayer1";
-            this.videoSourcePlayer1.Size = new System.Drawing.Size(320, 240);
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(322, 242);
             this.videoSourcePlayer1.TabIndex = 1;
             this.videoSourcePlayer1.Text = "videoSourcePlayer1";
             this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
             // 
-            // videoSourcePlayer2
+            // pictureBox1
             // 
-            this.videoSourcePlayer2.Location = new System.Drawing.Point(327, 43);
-            this.videoSourcePlayer2.Name = "videoSourcePlayer2";
-            this.videoSourcePlayer2.Size = new System.Drawing.Size(320, 240);
-            this.videoSourcePlayer2.TabIndex = 2;
-            this.videoSourcePlayer2.Text = "videoSourcePlayer2";
-            this.videoSourcePlayer2.VideoSource = null;
+            this.pictureBox1.Image = null;
+            this.pictureBox1.Location = new System.Drawing.Point(341, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(340, 242);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // sliderControl1
+            // 
+            this.sliderControl1.Location = new System.Drawing.Point(12, 290);
+            this.sliderControl1.ManipulatorPosition = 0F;
+            this.sliderControl1.Name = "sliderControl1";
+            this.sliderControl1.ResetPositionOnMouseRelease = false;
+            this.sliderControl1.Size = new System.Drawing.Size(225, 57);
+            this.sliderControl1.TabIndex = 3;
+            this.sliderControl1.Text = "sliderControl1";
+            this.sliderControl1.PositionChanged += new AForge.Controls.SliderControl.PositionChangedHandler(this.sliderControl1_PositionChanged);
+            // 
+            // colorSlider1
+            // 
+            this.colorSlider1.Location = new System.Drawing.Point(364, 384);
+            this.colorSlider1.Name = "colorSlider1";
+            this.colorSlider1.Size = new System.Drawing.Size(371, 63);
+            this.colorSlider1.TabIndex = 4;
+            this.colorSlider1.Text = "colorSlider1";
+            this.colorSlider1.ValuesChanged += new System.EventHandler(this.colorSlider1_ValuesChanged);
+            // 
+            // sliderControl2
+            // 
+            this.sliderControl2.Location = new System.Drawing.Point(12, 353);
+            this.sliderControl2.ManipulatorPosition = 0F;
+            this.sliderControl2.Name = "sliderControl2";
+            this.sliderControl2.ResetPositionOnMouseRelease = false;
+            this.sliderControl2.Size = new System.Drawing.Size(225, 57);
+            this.sliderControl2.TabIndex = 5;
+            this.sliderControl2.Text = "sliderControl2";
+            this.sliderControl2.PositionChanged += new AForge.Controls.SliderControl.PositionChangedHandler(this.sliderControl2_PositionChanged);
+            // 
+            // sliderControl3
+            // 
+            this.sliderControl3.Location = new System.Drawing.Point(760, 117);
+            this.sliderControl3.ManipulatorPosition = 0F;
+            this.sliderControl3.Name = "sliderControl3";
+            this.sliderControl3.ResetPositionOnMouseRelease = false;
+            this.sliderControl3.Size = new System.Drawing.Size(225, 57);
+            this.sliderControl3.TabIndex = 7;
+            this.sliderControl3.Text = "sliderControl3";
+            this.sliderControl3.PositionChanged += new AForge.Controls.SliderControl.PositionChangedHandler(this.sliderControl3_PositionChanged);
+            // 
+            // sliderControl4
+            // 
+            this.sliderControl4.Location = new System.Drawing.Point(760, 54);
+            this.sliderControl4.ManipulatorPosition = 0F;
+            this.sliderControl4.Name = "sliderControl4";
+            this.sliderControl4.ResetPositionOnMouseRelease = false;
+            this.sliderControl4.Size = new System.Drawing.Size(225, 57);
+            this.sliderControl4.TabIndex = 6;
+            this.sliderControl4.Text = "sliderControl4";
+            this.sliderControl4.PositionChanged += new AForge.Controls.SliderControl.PositionChangedHandler(this.sliderControl4_PositionChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.videoSourcePlayer2);
+            this.ClientSize = new System.Drawing.Size(1298, 561);
+            this.Controls.Add(this.sliderControl3);
+            this.Controls.Add(this.sliderControl4);
+            this.Controls.Add(this.sliderControl2);
+            this.Controls.Add(this.colorSlider1);
+            this.Controls.Add(this.sliderControl1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.videoSourcePlayer1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "RobovatorDemo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -85,7 +153,12 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
         private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
-        private AForge.Controls.VideoSourcePlayer videoSourcePlayer2;
+        private AForge.Controls.PictureBox pictureBox1;
+        private AForge.Controls.SliderControl sliderControl1;
+        private AForge.Controls.ColorSlider colorSlider1;
+        private AForge.Controls.SliderControl sliderControl2;
+        private AForge.Controls.SliderControl sliderControl3;
+        private AForge.Controls.SliderControl sliderControl4;
     }
 }
 
--- RobovatorDemo/Form1.cs
+++ RobovatorDemo/Form1.cs
@@ -10,27 +10,118 @@ using System.Windows.Forms;
 using AForge;
 using AForge.Video.DirectShow;
 using AForge.Controls;
+using AForge.Imaging.Filters;
+using System.Drawing.Imaging;
+using AForge.Imaging;
+using AForge.Video;
 
 namespace RobovatorDemo
 {
     public partial class Form1 : Form
     {
+        private YCbCrFiltering filter;
+        private Grayscale grayscaleFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+        private BlobCounter blobCounter = new BlobCounter();
+        private float maxCb;
+        private float minCb;
+        private float maxCr;
+        private float minCr;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            filter = new YCbCrFiltering();
+
+            blobCounter.MinWidth = 30;
+            blobCounter.MinHeight = 30;
+            blobCounter.FilterBlobs = true;
+            blobCounter.ObjectsOrder = ObjectsOrder.Size;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            openFileDialog1.Filter = "avi files (*.avi)|*.avi|All files (*.*)|*.*";
+            openFileDialog1.Filter = "All files (*.*)|*.*";
             openFileDialog1.RestoreDirectory = true;
             openFileDialog1.ShowDialog();
 
             FileVideoSource file = new FileVideoSource(openFileDialog1.FileName);
-            videoSourcePlayer1.AutoSizeControl = false;
+
             videoSourcePlayer1.VideoSource = file;
             videoSourcePlayer1.Start();
+            //videoSourcePlayer2.VideoSource = asyncSource;
+            //videoSourcePlayer2.Start();
+        }
+
+        int tmp = 0;
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            videoSourcePlayer1.Stop();
+        }
+
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+
+            //if (tmp <= 1)
+            //{
+            //    tmp++;
+            //    return;
+            //}
+            //tmp = 0;
+
+            Bitmap tmpImg = (Bitmap)image.Clone();
+            
+            filter.Y = new Range(0, 1);
+            filter.Cb = new Range(minCb, maxCb);
+            filter.Cr = new Range(minCr, maxCr);
+            filter.ApplyInPlace(tmpImg);
+            
+            //BitmapData objectsData = image.LockBits(new Rectangle(0, 0,
+            //    image.Width, image.Height), ImageLockMode.ReadOnly, image.PixelFormat);
+            //UnmanagedImage grayImage = grayscaleFilter.Apply(new UnmanagedImage(objectsData));
+            //image.UnlockBits(objectsData);
+
+
+            blobCounter.ProcessImage(tmpImg);
+            Rectangle[] rects = blobCounter.GetObjectsRectangles();
+            Graphics g = Graphics.FromImage(tmpImg);
+            Pen pen = new Pen(Color.FromArgb(160, 255, 160), 5);
+            foreach (Rectangle recs in rects)
+                if (rects.Length > 0)
+                    g.DrawRectangle(pen, rects[0]);
+            g.Dispose();
+            pictureBox1.Image = tmpImg;
 
         }
+
+        private void sliderControl1_PositionChanged(object sender, float position)
+        {
+            minCb = position * 500;
+        }
+
+        private void colorSlider1_ValuesChanged(object sender, EventArgs e)
+        {
+            int tmp = 0;
+
+        }
+
+        private void sliderControl2_PositionChanged(object sender, float position)
+        {
+            maxCb = position * 500;
+        }
+
+        private void sliderControl4_PositionChanged(object sender, float position)
+        {
+            minCr = position * 500;
+        }
+
+        private void sliderControl3_PositionChanged(object sender, float position)
+        {
+            maxCr = position * 500;
+        }
+
     }
 }
