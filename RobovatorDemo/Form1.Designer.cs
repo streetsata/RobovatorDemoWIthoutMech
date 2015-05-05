538193b06fdeab9f319b6f390d7c6eabf5ac8890
Serhiy Artemenko <serhiy_artemenko@outllok.com>
2015-05-05 07:15:47 +0300

    1

---
+++ RobovatorDemo/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace RobovatorDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.videoSourcePlayer2 = new AForge.Controls.VideoSourcePlayer();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(0, 43);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(320, 240);
+            this.videoSourcePlayer1.TabIndex = 1;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // videoSourcePlayer2
+            // 
+            this.videoSourcePlayer2.Location = new System.Drawing.Point(327, 43);
+            this.videoSourcePlayer2.Name = "videoSourcePlayer2";
+            this.videoSourcePlayer2.Size = new System.Drawing.Size(320, 240);
+            this.videoSourcePlayer2.TabIndex = 2;
+            this.videoSourcePlayer2.Text = "videoSourcePlayer2";
+            this.videoSourcePlayer2.VideoSource = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.videoSourcePlayer2);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "RobovatorDemo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer2;
+    }
+}
+
---
+++ RobovatorDemo/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using AForge;
+using AForge.Video.DirectShow;
+using AForge.Controls;
+
+namespace RobovatorDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "avi files (*.avi)|*.avi|All files (*.*)|*.*";
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.ShowDialog();
+
+            FileVideoSource file = new FileVideoSource(openFileDialog1.FileName);
+            videoSourcePlayer1.AutoSizeControl = false;
+            videoSourcePlayer1.VideoSource = file;
+            videoSourcePlayer1.Start();
+
+        }
+    }
+}
